c6c2bae2915e57fddc14debd2317918b5f59b3ee
Sorry <5646769+sorry1@user.noreply.gitee.com>
2025-11-17 14:12:00 +0800

    feat: 修改pm2命令

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  apps: [
+    {
+      name: "npm-registry",
+      script: "./index.js",
+      instances: 4,
+      exec_mode: "cluster",
+      out_file: "/dev/null",
+      error_file: "/dev/null",
+      log_file: "/dev/null"
+    }
+  ]
+};
